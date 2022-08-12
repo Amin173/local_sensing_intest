049f7fe8880d1907fed2f2afdea5c1a38b6f4013
dcanonesbonham <dcanonesbonham@iit.edu>
2022-08-12 16:13:50 +0000

    More examples

--- scripts/simulated_robot_tcp.py
+++ scripts/simulated_robot_tcp.py
@@ -40,7 +40,7 @@ def getTime_data(time, num_bots, data):
     file_dt = data[1, 0] - data[0, 0]
 
     # calculate index for given time and timestep:
-    idx = int(np.round(time / file_dt, 0))
+    idx = int(np.round(time / file_dt * 1, 0))
 
     tmp = data[idx, 1:].reshape((num_bots, 7))
 
